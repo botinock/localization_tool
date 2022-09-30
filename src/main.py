e88104a1eb1f6e903d29cf4a2d4c518078af7649
unknown <kirillreznikovkirill@gmail.com>
2022-09-30 04:04:07 +0300

    add file&text processing; add some buttons; main text as list widget

--- src/main.py
+++ src/main.py
@@ -1,34 +1,30 @@
 import sys
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLineEdit, QPlainTextEdit, QLabel
-from PyQt6.QtWidgets import QVBoxLayout, QHBoxLayout, QSizePolicy
+from PyQt6.QtGui import QAction, QFont, QIcon
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLineEdit, QLabel, QPushButton, QMenuBar
+from PyQt6.QtWidgets import QListWidget
+from PyQt6.QtWidgets import QVBoxLayout, QHBoxLayout
 
+from text_processing import TextProcessing
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, file):
         super().__init__()
         self.setWindowTitle("Localization Tool")
+        menu = QMenuBar()
+        menu.addAction("File")
+        menu.addAction("Font")
+        menu.addAction("Night Mode")
+        self.setMenuBar(menu)
 
         layout = QVBoxLayout()
 
-        self.plain_text = QPlainTextEdit(f'''[JP:1]"　食後はテレビを見る気力もなく、…部屋で布団に潜りながら…陰鬱な考えに頭をめぐらせていた…。"
-[EN:1]"After dinner, without the energy to watch TV... I climbed into the futon in my bedroom... and let my head be overtaken by gloomy thoughts..."
-[UA:1]"After dinner, without the energy to watch TV... I climbed into the futon in my bedroom... and let my head be overtaken by gloomy thoughts..."
-[JP:2]"　梨花ちゃんに洗いざらいの全てを話した。"
-[EN:2]"I told Rika-chan anything and everything."
-[UA:2]"I told Rika-chan anything and everything."
+        self.plain_text = QListWidget()
 
-[JP]"詩音"
-[EN]"Shion"
-[UA]"Shion"
-[JP:3]"「では……昨日の続きを話します。"
-[EN:3]"\"Then... let's continue from where we left off."
-[UA:3]"\"Then... let's continue from where we left off."
-[JP:4]"……もう怒らないで聞いてくれますよね？」"
-[EN:4]" ...You'll listen without getting angry this time, right?\""
-[UA:4]" ...You'll listen without getting angry this time, right?\""''')
+        tp = TextProcessing()
+        text = tp.read(file)
+        self.plain_text.addItems([''.join(line).strip('\n') for line in text])
         self.jp_line = QLineEdit("……もう怒らないで聞いてくれますよね？」")
         self.en_line = QLineEdit("...You'll listen without getting angry this time, right?")
         self.ua_line = QLineEdit("...Тепер послухай будь ласка спокійно, добре?")
@@ -40,19 +36,26 @@ class MainWindow(QMainWindow):
         jp_layout = QHBoxLayout()
         en_layout = QHBoxLayout()
         ua_layout = QHBoxLayout()
+        
+        self.back_button = QPushButton("Back")
+        self.save_button = QPushButton("Save")
+        self.next_button = QPushButton("Next")
 
         jp_layout.addWidget(jp_flag)
         jp_layout.addWidget(self.jp_line)
+        jp_layout.addWidget(self.back_button)
+
         en_layout.addWidget(en_flag)
         en_layout.addWidget(self.en_line)
+        en_layout.addWidget(self.save_button)
+
         ua_layout.addWidget(ua_flag)
         ua_layout.addWidget(self.ua_line)
+        ua_layout.addWidget(self.next_button)
 
-        self.plain_text.setReadOnly(True)
         self.jp_line.setReadOnly(True)
         self.en_line.setReadOnly(True)
 
-        # layout.addStretch()
         layout.addWidget(self.plain_text)
         layout.addLayout(jp_layout)
         layout.addLayout(en_layout)
@@ -62,12 +65,12 @@ class MainWindow(QMainWindow):
         widget.setLayout(layout)
         self.setCentralWidget(widget)
         
-
-
-
-app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
-app.exec()
+if __name__ == '__main__':
+    app = QApplication([])
+    try:
+        file = sys.argv[1]
+    except:
+        file = ''
+    window = MainWindow(file)
+    window.show()
+    app.exec()
---
+++ src/text_processing.py
@@ -0,0 +1,48 @@
+import sys
+import re
+
+class TextProcessing:
+    def __init__(self) -> None:
+        self.jp_list: list[str] = []
+        self.en_list: list[str] = []
+        self.ua_list: list[str] = []
+        self.regex = re.compile(r"\"(?:\\\")?\s?(.*?)(?:\\\")?\"")
+    
+    def read(self, file: str = ''):
+        if file != '':
+            with open(file, 'r', encoding='UTF-8') as f:
+                text_list = f.readlines()
+        else:
+            text_list = []
+        
+        for line in text_list:
+            match line[1:3]:
+                case "JP":
+                    self.jp_list.append(line)
+                case "EN":
+                    self.en_list.append(line)
+                case "UA":
+                    self.ua_list.append(line)
+        return self.make_text()
+
+    def make_text(self):
+        self.text = list(zip(self.jp_list, self.en_list, self.ua_list))
+        return self.text
+
+    def update_ua(self, line: str, idx: int):
+        self.ua_list[idx] = self.ua_list[idx].replace(
+            self.parse_string(self.ua_list[idx]), line)
+        return self.make_text()
+
+    def parse_string(self, line: str) -> str:
+        return self.regex.search(line).group(1)
+        
+
+if __name__ == '__main__':
+    try:
+        file = sys.argv[1]
+        tp = TextProcessing()
+        print(tp.read(file))
+        print(tp.update_ua("ЦЕ ПЕРЕКЛАД ЦЕ ПЕРЕКЛАД ЦЕ ПЕРЕКЛАД", -1))
+    except Exception as e:
+        print(e)
